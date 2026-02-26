a36e1ec2867cd419c1f33fe3228a4788c80c811f
habeeba148 <es.habiba.shady2023@alexu.edu.eg>
2026-02-26 22:56:14 +0200

    calculating time

--- s.sort.py
+++ s.sort.py
@@ -1,4 +1,6 @@
+import time
 def selection_sort(arr):
+    starttime=time.time()
     n=len(arr)
     for i in range (n-1):
         min=arr[i]
@@ -8,4 +10,6 @@ def selection_sort(arr):
                 minindex=j
                 min=arr[j]
         arr[i], arr[minindex] = arr[minindex], arr[i]
+    endtime=time.time()
+    print("Running time for bubble sort is "+str((endtime-starttime)*1000)+"ms")
     return arr
